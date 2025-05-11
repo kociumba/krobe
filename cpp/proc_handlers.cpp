749b99fa55efbfbe6fe47262c5a15855160bfd7d
kociumba <79542688+kociumba@users.noreply.github.com>
2025-05-11 14:03:44 +0200

    fix c++ proc_handlers

--- cpp/proc_handlers.cpp
+++ cpp/proc_handlers.cpp
@@ -6,36 +6,35 @@
 #pragma comment(lib, "user32.lib")
 
 // Get the process ID of a running process
-extern "C" DWORD get_process_id(const char *process_name) {
-    DWORD process_id = 0;
+DWORD get_process_id(const wchar_t* process_name) {
+	DWORD process_id = 0;
 
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-    if (snapshot == INVALID_HANDLE_VALUE) {
-        return process_id;
-    }
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (snapshot == INVALID_HANDLE_VALUE) {
+		return process_id;
+	}
 
-    // printf("valid snapshot");
+	PROCESSENTRY32W entry = {};
+	entry.dwSize = sizeof(decltype(entry));
 
-    PROCESSENTRY32 entry = {};
-    entry.dwSize = sizeof(decltype(entry));
+	if (Process32FirstW(snapshot, &entry) == TRUE) {
+		// Check if the first handle is the one we want
+		if (_wcsicmp(process_name, entry.szExeFile) == 0) {
+			process_id = entry.th32ProcessID;
+		}
+		else {
+			while (Process32NextW(snapshot, &entry) == TRUE) {
+				if (_wcsicmp(process_name, entry.szExeFile) == 0) {
+					process_id = entry.th32ProcessID;
+					break;
+				}
+			}
+		}
+	}
 
-    if (Process32First(snapshot, &entry) == TRUE) {
-        // Check if the first handle is the one we want
-        if (_stricmp(process_name, entry.szExeFile) == 0) {
-            process_id = entry.th32ProcessID;
-        } else {
-            while (Process32Next(snapshot, &entry) == TRUE) {
-                if (_stricmp(process_name, entry.szExeFile) == 0) {
-                    process_id = entry.th32ProcessID;
-                    break;
-                }
-            }
-        }
-    }
+	CloseHandle(snapshot);
 
-    CloseHandle(snapshot);
-
-    return process_id;
+	return process_id;
 }
 
 BOOL CALLBACK EnumWindowsProc(HWND current_hwnd, LPARAM lparam) {
@@ -64,32 +63,33 @@ extern "C" HWND get_hwnd(DWORD process_id) {
 }
 
 // Get the base address of a module loaded in a process
-extern "C" std::uintptr_t get_module_base(const DWORD pid, const char *module_name) {
-    std::uintptr_t module_base = 0;
+std::uintptr_t get_module_base(const DWORD pid, const wchar_t* module_name) {
+	std::uintptr_t module_base = 0;
 
-    // Snap shot of process' modules (dlls)
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
-    if (snapshot == INVALID_HANDLE_VALUE) {
-        return module_base;
-    }
+	// Snap shot of process' modules (dlls)
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
+	if (snapshot == INVALID_HANDLE_VALUE) {
+		return module_base;
+	}
 
-    MODULEENTRY32 entry = {};
-    entry.dwSize = sizeof(decltype(entry));
+	MODULEENTRY32W entry = {};
+	entry.dwSize = sizeof(decltype(entry));
 
-    if (Module32First(snapshot, &entry) == TRUE) {
-        if (strstr(module_name, entry.szModule) == nullptr) {
-            module_base = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
-        } else {
-            while (Module32Next(snapshot, &entry) == TRUE) {
-                if (strstr(module_name, entry.szModule) == nullptr) {
-                    module_base = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
-                    break;
-                }
-            }
-        }
-    }
+	if (Module32FirstW(snapshot, &entry) == TRUE) {
+		if (wcsstr(module_name, entry.szModule) != nullptr) { // the logic is probably fixed here
+			module_base = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+		}
+		else {
+			while (Module32NextW(snapshot, &entry) == TRUE) {
+				if (wcsstr(module_name, entry.szModule) != nullptr) {
+					module_base = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+					break;
+				}
+			}
+		}
+	}
 
-    CloseHandle(snapshot);
+	CloseHandle(snapshot);
 
-    return module_base;
+	return module_base;
 }
